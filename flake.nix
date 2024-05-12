f7f7395fa662634c0826c589114f81823974cc21
Knarkzel <knarkzel@gmail.com>
2024-05-12 22:41:38 +0200

    idk

--- flake.nix
+++ flake.nix
@@ -4,20 +4,26 @@
   };
 
   outputs = { nixpkgs, ... }: let
+    src = ./.;
+    pname = "studio";
     system = "x86_64-linux";
+    version = "0.1.0";
+    MIX_ENV = "prod";
     pkgs = import nixpkgs {
       inherit system;
     };
     beamPackages = pkgs.beamPackages;
   in {
     packages.${system}.default = beamPackages.mixRelease {
-      src = ./.;
-      pname = "studio";
-      version = "0.1.0";
-      MIX_ENV = "prod";
+      inherit src pname version MIX_ENV;
       mixNixDeps = import ./deps.nix {
         inherit (pkgs) lib beamPackages;
       };
+      postBuild = ''
+        mkdir -p $out/lib/${pname}-${version}/priv
+        cp -r priv $out/lib/${pname}-${version}/priv
+        cp -r assets $out/lib/${pname}-${version}/priv/static
+      '';
     };
   };
 }
